1040bf076f55438b28fe649864a18c64e4615c40
dowglasSantos <bittencourtdouglas41@gmail.com>
2025-01-12 16:24:57 -0300

    subindo os arquivos do projeto de variaveis do java

---
+++ src/VariablesJava.java
@@ -0,0 +1,74 @@
+public class VariablesJava {
+    private int variavelInt;
+    private byte variavelByte;
+    private long variavelLong;
+    private short variavelShort;
+    private float variavelFloat;
+    private double variavelDouble;
+    private String variavelString;
+    private boolean variavelBoolean;
+
+    public int getVariavelInt() {
+        return variavelInt;
+    }
+
+    public void setVariavelInt(int variavelInt) {
+        this.variavelInt = variavelInt;
+    }
+
+    public byte getVariavelByte() {
+        return variavelByte;
+    }
+
+    public void setVariavelByte(byte variavelByte) {
+        this.variavelByte = variavelByte;
+    }
+
+    public long getVariavelLong() {
+        return variavelLong;
+    }
+
+    public void setVariavelLong(long variavelLong) {
+        this.variavelLong = variavelLong;
+    }
+
+    public short getVariavelShort() {
+        return variavelShort;
+    }
+
+    public void setVariavelShort(short variavelShort) {
+        this.variavelShort = variavelShort;
+    }
+
+    public float getVariavelFloat() {
+        return variavelFloat;
+    }
+
+    public void setVariavelFloat(float variavelFloat) {
+        this.variavelFloat = variavelFloat;
+    }
+
+    public double getVariavelDouble() {
+        return variavelDouble;
+    }
+
+    public void setVariavelDouble(double variavelDouble) {
+        this.variavelDouble = variavelDouble;
+    }
+
+    public String getVariavelString() {
+        return variavelString;
+    }
+
+    public void setVariavelString(String variavelString) {
+        this.variavelString = variavelString;
+    }
+
+    public boolean getVariavelBoolean() {
+        return variavelBoolean;
+    }
+
+    public void setVariavelBoolean(boolean variavelBoolean) {
+        this.variavelBoolean = variavelBoolean;
+    }
+}
